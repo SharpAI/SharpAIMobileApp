a8cf389b346eaa790ac1dec34e333af3d3e96849
Simba Zhang <solderzzc@gmail.com>
2021-02-15 11:35:54 -0800

    to build with release flag, bump to 3.3.2

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.sharpai.everywhere',
-  version: '3.3.1',
+  version: '3.3.2',
   name: 'sharpai',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
