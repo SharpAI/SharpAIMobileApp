739b9c92429218d242b61de9b74bc911f65d98a6
Simba Zhang <solderzzc@gmail.com>
2021-03-21 16:24:52 -0700

    bump version to 3.4.1

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'org.sharpai.everywhere',
-  version: '3.4.0',
+  version: '3.4.1',
   name: 'sharpai',
   description: 'Share everything with everyone',
   author: 'hotShare Design Team',
